1fecead5791048216ff8b6ccfc3a57a8c5e32e9e
jakiviki <javikido40@gmail>
2020-07-01 12:13:17 +0200

    empiezo el proyecoto

---
+++ app/src/main/java/com/example/complvtvm/ListViewGuerrero.java
@@ -0,0 +1,14 @@
+package com.example.complvtvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListViewGuerrero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_guerrero);
+    }
+}
---
+++ app/src/main/java/com/example/complvtvm/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.complvtvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.icu.text.CaseMap;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
